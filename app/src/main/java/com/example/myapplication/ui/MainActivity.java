86667c28688d996b3683d5b08c6c5c11320d159f
Konstantin <mr.ks83@mail.ru>
2017-09-19 01:56:09 +0300

    сообщение приходит, осталось  поймать и распарсить

--- app/src/main/java/com/example/myapplication/ui/MainActivity.java
+++ app/src/main/java/com/example/myapplication/ui/MainActivity.java
@@ -1,19 +1,32 @@
 package com.example.myapplication.ui;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import com.example.myapplication.R;
 import com.example.myapplication.model.myUser;
 import com.example.myapplication.ui.gallery.Fetcher;
 import com.example.myapplication.ui.gallery.MyGalleryFrafment;
 import com.example.myapplication.ui.list.MyListFragment;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+
 import java.util.ArrayList;
 import java.util.List;
 import io.realm.Realm;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     Realm realm;
     private List<myUser> mItems = new ArrayList<>();
@@ -33,7 +46,19 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.list_container, new MyListFragment())
                     .commit();
         }
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId = getString(R.string.default_notification_channel_id);
+            String channelName = getString(R.string.default_notification_channel_name);
+            NotificationManager notificationManager =
+                    getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(new NotificationChannel(channelId,
+                    channelName, NotificationManager.IMPORTANCE_LOW));
+        }
+        FirebaseApp.initializeApp(this);
+        String token = FirebaseInstanceId.getInstance().getToken();
+       Log.d("myToken", token);
+        Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
+        FirebaseMessaging.getInstance().subscribeToTopic("news");
     }
 
     private class FetchItemTask extends AsyncTask<Void, Void, List<myUser>> {
